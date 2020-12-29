b617541d178f9587f936ab83ede42d78630c28fb
daryakaviani <daryakavi@gmail.com>
2020-12-28 19:45:13 -0800

    Complicated version

--- script.py
+++ script.py
@@ -27,10 +27,54 @@ def stats(path, movement):
             print(filename)
             new_path = os.path.join(path, filename)
             img = cv2.imread(new_path)
-            img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            text = pytesseract.image_to_string(img_rgb)
-            lowercase_text = text.lower()
+            # Convert the image to gray scale 
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+            # Performing OTSU threshold 
+            ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+            # Specify structure shape and kernel size.  
+            # Kernel size increases or decreases the area  
+            # of the rectangle to be detected. 
+            # A smaller value like (10, 10) will detect  
+            # each word instead of a sentence. 
+            rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18)) 
+            # Appplying dilation on the threshold image 
+            dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1) 
+            # Finding contours 
+            contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
+            # Creating a copy of image 
+            im2 = img.copy()
+            # A text file is created and flushed 
+            # file = open("recognized.txt", "w+") 
+            # file.write("") 
+            # file.close() 
+            text = ""
+            lowercase_text = ""
+              
+            # Looping through the identified contours 
+            # Then rectangular part is cropped and passed on 
+            # to pytesseract for extracting text from it 
+            # Extracted text is then written into the text file 
+            for cnt in contours: 
+                x, y, w, h = cv2.boundingRect(cnt) 
+                # Drawing a rectangle on copied image 
+                rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2) 
+                # Cropping the text block for giving input to OCR 
+                cropped = im2[y:y + h, x:x + w] 
+                # # Open the file in append mode 
+                # file = open("recognized.txt", "a") 
+                # Apply OCR on the cropped image 
+                text += pytesseract.image_to_string(cropped) + " "
+                lowercase_text += pytesseract.image_to_string(cropped).lower() + " "
+                # # Appending the text into file 
+                # file.write(text) 
+                # file.write("\n") 
+                # Close the file 
+                # file.close
             print(lowercase_text)
+            # img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            # text = pytesseract.image_to_string(img_rgb)
+            # lowercase_text = text.lower()
+            # print(lowercase_text)
 
             # Get the JSON for the image
             json_filename = filename[:-4]
@@ -63,6 +107,7 @@ def stats(path, movement):
                     for word in action_words:
                         if word in lowercase_text:
                             num_action_words[year] += 1
+
     print("Stats for: " + movement +":")
     overall_num_total_likes = overall_num_total_comments = overall_num_infographic_likes = overall_num_infographic_comments = overall_num_images_with_text = overall_num_images = 0
     for year in years:
